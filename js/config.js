959d88662d30879a1cbd7b9c79153d620e379bb2
Tasneem <152633167+Tasneem-oda@users.noreply.github.com>
2026-09-03 17:46:53 +0300

    Export CONFIG object from config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
+export const CONFIG = {
     SUPABASE_URL: 'https://qdpnupgqvjxlrmwwgmij.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkcG51cGdxdmp4bHJtd3dnbWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NjczMjUsImV4cCI6MjA4NTU0MzMyNX0.gaWPEgrhIQJzZLVoo7x1hnS-63ZJPZN96Xb3WpqApik',
     N8N_WEBHOOK_BASE: 'https://tasneemahmed-n8n.hf.space/webhook'
